58e09d1cc6a9dce9ba9759410a487c6972a480b1
Saša Stojković <128098274+Sasa1307@users.noreply.github.com>
2023-03-17 17:58:41 +0100

    Create change_button.js

---
+++ change_button.js
@@ -0,0 +1,9 @@
+function change_active_Function(ide, ime1, ime2)
+{
+document.getElementById(ide).src=ime2;
+}
+
+function change_passive_Function(ide, ime1, ime2)
+{
+document.getElementById(ide).src=ime1;
+}
